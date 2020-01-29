37eaf6e86e2929c5c68a1cb900f8464d399e0b4e
jacksonsr45 <31199441+jacksonsr45@users.noreply.github.com>
2020-01-28 23:44:51 -0300

    Jackson
    Up ADM Blog

--- src/meuBlog/urls.py
+++ src/meuBlog/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 from .views import index
 from .views import post
@@ -11,6 +12,7 @@ from .views import contact
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('post/', post),
     path('about/', about),
     path('contact/', contact),
